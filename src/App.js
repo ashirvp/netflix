68adbdf67287c7f13baa3668e682affd9737408e
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-11-11 10:33:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import "./Components/NavBar/NavBar.css"
+import "./App.css"
+import Banner from './Components/Banner/Banner';
+
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar/>
+     <Banner/>
+    </div>
+  );
+}
+
+export default App;
