ba776ca668cd8093fd7ddf67c83ae4a2f23d6847
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-11-17 13:10:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import NavBar from './Components/NavBar/NavBar';
 import "./App.css"
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-import {originals,action,horror, comedy, romance, treding} from './urls'
+import { originals, action, horror, comedy, romance, treding } from './urls'
 
 
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
-     <Banner/>
-     <RowPost url={originals} title="Netfix Originals"/>
-     <RowPost url={treding} title="Trending Movies" isSmall={true}/>
-     <RowPost url={action} title="Action" isSmall={true}/>
-     <RowPost url={horror} title="Horror" isSmall={true}/>
-     <RowPost url={romance} title="Romance" isSmall={true}/>
-     <RowPost url={comedy} title="Comedy" isSmall={true}/>
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title="Netfix Originals" />
+      <RowPost url={treding} title="Trending Movies" isSmall={true} />
+      <RowPost url={action} title="Action" isSmall={true} />
+      <RowPost url={horror} title="Horror" isSmall={true} />
+      <RowPost url={romance} title="Romance" isSmall={true} />
+      <RowPost url={comedy} title="Comedy" isSmall={true} />
     </div>
   );
 }
